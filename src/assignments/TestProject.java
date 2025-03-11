64cdcca09e27289ec1e95737522b7bc96ef976d5
Nanthini Kumari <nanthini.kumari11121991@gmail.com>
2025-03-11 09:28:35 +0530

    Assignment 1 completed

---
+++ src/assignments/TestProject.java
@@ -0,0 +1,16 @@
+package assignments;
+
+public class TestProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// print me sum of two numbers; 
+		System.out.print(1+1);
+        System.out.println(1+1);
+        System.out.printf("Hello, %s! You have %d new messages are %b.%n", "Alice", 5, true);
+        System.out.format("The value of pi is approximately %.2f%n", 3.14159);
+
+	}
+
+	}
+
